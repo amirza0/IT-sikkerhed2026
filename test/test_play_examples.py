45560a6caf373af226cbc12bd43a67b4cd4cf5f2
Amnab <amna.mirza3@gmail.com>
2026-06-22 01:42:23 +0200

    Add test strategies and documentation

--- test/test_play_examples.py
+++ test/test_play_examples.py
@@ -13,3 +13,17 @@ def test_leg_fejler_med_vilje():
 @pytest.mark.skip(reason="Leg: fjern skip for at se testen crashe")
 def test_leg_crasher_med_vilje():
     raise RuntimeError("Test crasher med vilje")
+
+#import pytest
+
+
+#def test_leg_passer():
+ #   assert "zealand".upper() == "ZEALAND"
+
+
+#def test_leg_fejler_med_vilje():
+ #   assert 2 + 2 == 5
+
+
+#def test_leg_crasher_med_vilje():
+ #   raise RuntimeError("Test crasher med vilje")
